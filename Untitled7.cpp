b08e07e2fecb3bcfb805491fdea68542cbedebef
JeanMC <jmichael1803@hotmail.com>
2017-10-20 21:47:32 -0700

    Se removio cin.get

--- Untitled7.cpp
+++ Untitled7.cpp
@@ -1,8 +1,7 @@
-#define suma(a,b) a+b 
+#define suma(a,b) a+b
 #define resta(a,b) a-b
 #define multiplicar(a,b) a*b
 #define division(a,b) a/b
-
 #include <iostream>
 
 int i = 4;
@@ -19,18 +18,16 @@ using namespace std;
 int main()
 
 {
-	
+
  sum = suma (i,j);
  res = resta (i,j);
  multi = multiplicar (i,j);
  div = division(i,j);
-    
+
 cout <<  "la suma es: " << sum << endl;
 cout <<  "la resta es: " << res << endl;
 cout <<  "la multiplicacion  es: " << multi << endl;
 cout <<  "el cociente  es: " << div << endl;
-
-cin.get();
 return 0;
 
 }
