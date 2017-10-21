eb871bc26bf8d12010ac8f237c9db75dbefc284e
JeanMC <jmichael1803@hotmail.com>
2017-10-20 21:40:20 -0700

    Ejemplo

---
+++ Untitled7.cpp
@@ -0,0 +1,36 @@
+#define suma(a,b) a+b 
+#define resta(a,b) a-b
+#define multiplicar(a,b) a*b
+#define division(a,b) a/b
+
+#include <iostream>
+
+int i = 4;
+int j = 2;
+
+int sum;
+int res;
+int multi;
+int div;
+
+using namespace std;
+
+
+int main()
+
+{
+	
+ sum = suma (i,j);
+ res = resta (i,j);
+ multi = multiplicar (i,j);
+ div = division(i,j);
+    
+cout <<  "la suma es: " << sum << endl;
+cout <<  "la resta es: " << res << endl;
+cout <<  "la multiplicacion  es: " << multi << endl;
+cout <<  "el cociente  es: " << div << endl;
+
+cin.get();
+return 0;
+
+}
